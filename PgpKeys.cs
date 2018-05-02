978f02b80bd4407af72e5bc28ef820bf2ac50024
Philip Fulgham <philfulgham@gmail.com>
2018-05-01 20:10:58 -0400

    Starting point commit for demo

--- PgpKeys.cs
+++ PgpKeys.cs
@@ -10,39 +10,9 @@ namespace SharpObjects
 {
 	public class PgpKeys
 	{
-		private bool _toEncrypt; // true == for encryption, false == for decryption
-
-		public PgpPublicKey PublicKey { get; set; }
-		public PgpPrivateKey PrivateKey { get; set; }
-		public PgpSecretKey SecretKey { get; set; }
-
 		// Constructor; if you only want to decrypt, you'd only need publicKeyStream
-		public PgpKeys(Stream publicKeyStream, Stream privateKeyStream = null, string passPhrase = null, bool toEncrypt = true)
-		{
-			_toEncrypt = toEncrypt;
-			PublicKey = ReadPublicKey(publicKeyStream);
-			if (privateKeyStream != null)
-			{
-				SecretKey = ReadSecretKey(privateKeyStream);
-				PrivateKey = ReadPrivateKey(passPhrase);
-			}
-		}
 
 		// Get the first public key from a key ring bundle
-		private PgpPublicKey GetFirstPublicKey(PgpPublicKeyRingBundle publicKeyRingBundle)
-		{
-			foreach (PgpPublicKeyRing kRing in publicKeyRingBundle.GetKeyRings())
-			{
-				PgpPublicKey key = kRing.GetPublicKeys()
-					.Cast<PgpPublicKey>()
-					.FirstOrDefault(k => k.IsEncryptionKey);
-				if (key != null)
-				{
-					return key;
-				}
-			}
-			return null;
-		}
 
 		// Pre-built methods for getting the first or last secret key from a key ring bundle.
 		// Depending on your exact configuration, you may need to play with LINQ a bit
@@ -50,68 +20,11 @@ namespace SharpObjects
 		// If you get a PgpException: exception decrypting secret key
 		// with an inner InvalidCipherTextException: unknown block type
 		// that's probably what's causing it. For me, it was trial and error.
-		private PgpSecretKey GetFirstSecretKey(PgpSecretKeyRingBundle secretKeyRingBundle)
-		{
-			foreach (PgpSecretKeyRing kRing in secretKeyRingBundle.GetKeyRings())
-			{
-				PgpSecretKey key = kRing.GetSecretKeys()
-					.Cast<PgpSecretKey>()
-					.FirstOrDefault(k => k.IsSigningKey);
-				if (key != null)
-				{
-					return key;
-				}
-			}
-			return null;
-		}
-
-		private PgpSecretKey GetLastSecretKey(PgpSecretKeyRingBundle secretKeyRingBundle)
-		{
-			return (from PgpSecretKeyRing kRing in secretKeyRingBundle.GetKeyRings()
-					select kRing.GetSecretKeys().Cast<PgpSecretKey>()
-					.LastOrDefault(k => k.IsSigningKey))
-					.LastOrDefault(key => key != null);
-		}
 
 		// Read the public key into memory for encrypting data
-		private PgpPublicKey ReadPublicKey(Stream publicKeyStream)
-		{
-			using (Stream inputStream = PgpUtilities.GetDecoderStream(publicKeyStream))
-			{
-				PgpPublicKeyRingBundle publicKeyRingBundle = new PgpPublicKeyRingBundle(inputStream);
-				PgpPublicKey foundKey = GetFirstPublicKey(publicKeyRingBundle);
-				if (foundKey != null)
-				{
-					return foundKey;
-				}
-			}
-			throw new ArgumentException("No encryption key found in public key ring.");
-		}
 
 		// Private key is derived from secret key using passphrase
-		private PgpPrivateKey ReadPrivateKey(string passPhrase)
-		{
-			PgpPrivateKey privateKey = SecretKey.ExtractPrivateKey(passPhrase.ToCharArray());
-			if (privateKey != null)
-			{
-				return privateKey;
-			}
-			throw new ArgumentException("No private key found in secret key.");
-		}
 
 		// Secret key (private key outside Bouncy Castle) is used for decrypting and signing
-		private PgpSecretKey ReadSecretKey(Stream privateKeyStream)
-		{
-			using (Stream inputStream = PgpUtilities.GetDecoderStream(privateKeyStream))
-			{
-				PgpSecretKeyRingBundle secretKeyRingBundle = new PgpSecretKeyRingBundle(inputStream);
-				PgpSecretKey foundKey = GetFirstSecretKey(secretKeyRingBundle);
-				if (foundKey != null)
-				{
-					return foundKey;
-				}
-				throw new ArgumentException("Can't find signing key in key ring.");
-			}
-		}
 	}
 }
--- StreamEncryption.cs
+++ StreamEncryption.cs
@@ -13,123 +13,12 @@ namespace SharpObjects
 {
 	public class StreamEncryption
 	{
-		private const int BUFFER = 0x10000; // 65536, an arbitrary power of 2
-		private readonly PgpKeys _pgpKeys;
-
-		public StreamEncryption(PgpKeys keys)
-		{
-			if (keys == null)
-			{
-				throw new ArgumentNullException("keys", "keys object is null");
-			}
-			_pgpKeys = keys;
-		}
-
 		// Chain streams from input to PgpLiteralData to PgpEncryptedData
-		private Stream ChainEncryptedOut(Stream outputStream)
-		{
-			var generator = new PgpEncryptedDataGenerator(SymmetricKeyAlgorithmTag.Aes256, true, new SecureRandom());
-			generator.AddMethod(_pgpKeys.PublicKey);
-			return generator.Open(outputStream, new byte[BUFFER]);
-		}
-
-		private static Stream ChainLiteralOut(Stream encryptedOut, string fileName)
-		{
-			var generator = new PgpLiteralDataGenerator();
-			// fileName is just a data point, but some implementations check it for validation
-			return generator.Open(encryptedOut, PgpLiteralData.Utf8, fileName, DateTime.Now, new byte[BUFFER]);
-		}
 
 		// Decrypt method just needs input and output
-		public void Decrypt(Stream inputStream, Stream outputStream)
-		{
-			// Load input stream into PgpEncryptedDataList
-			var input = PgpUtilities.GetDecoderStream(inputStream);
-			var encryptedFactory = new PgpObjectFactory(input);
-			PgpEncryptedDataList dataList;
-			// Iterate until we hit the first PgpEncryptedDataList
-			PgpObject obj;
-			do {
-				obj = encryptedFactory.NextPgpObject();
-			} while (!(obj is PgpEncryptedDataList));
-			dataList = (PgpEncryptedDataList)obj;
-
-			// Use our private key to decrypt
-			PgpPublicKeyEncryptedData encryptedData =
-				dataList.GetEncryptedDataObjects().Cast<PgpPublicKeyEncryptedData>().First();
-			Stream decryptedData = encryptedData.GetDataStream(_pgpKeys.PrivateKey);
-			var decryptedFactory = new PgpObjectFactory(decryptedData);
-
-			PgpObject message = decryptedFactory.NextPgpObject();
-			// If the message starts with a signature, jump to the next object
-			if (message is PgpOnePassSignatureList)
-			{
-				message = decryptedFactory.NextPgpObject();
-			}
-			PgpLiteralData literalData = (PgpLiteralData)message;
-			// Pipe the literal data to our output stream
-			Stream decryptedLiteral = literalData.GetInputStream();
-			Streams.PipeAll(decryptedLiteral, outputStream);
-		}
 
 		// Encrypt method also uses fileName and a signing setting
-		public void Encrypt(string fileName, Stream inputStream, Stream outputStream, bool sign = false)
-		{
-			// Init this to null so the IDE doesn't give us warnings
-			PgpSignatureGenerator signatureGenerator = null;
-
-			// Encrypted stream is the outer layer, gets fed to the output stream
-			Stream encryptedOut = ChainEncryptedOut(outputStream);
-			if (sign)
-			{
-				signatureGenerator = InitSignatureGenerator(encryptedOut);
-			}
-
-			// Literal stream is the inner layer, gets fed to the encrypted stream
-			Stream literalOut = ChainLiteralOut(encryptedOut, fileName);
-			int length = 0;
-			byte[] buffer = new byte[BUFFER];
-			// Standard .NET stream reading/writing stuff
-			while ((length = inputStream.Read(buffer, 0, buffer.Length)) > 0)
-			{
-				literalOut.Write(buffer, 0, length);
-				if (sign)
-				{
-					// Keep the signature generator in sync
-					signatureGenerator.Update(buffer, 0, length);
-				}
-			}
-			literalOut.Close();
-
-			if (sign)
-			{
-				// Generate and apply the signature to the outer stream at the end
-				signatureGenerator.Generate().Encode(encryptedOut);
-			}
-			encryptedOut.Close();
-		}
 
 		// Signature generator builder is only needed if we're signing
-		private PgpSignatureGenerator InitSignatureGenerator(Stream streamToSign)
-		{
-			// Get our generator set up to match our keys
-			PublicKeyAlgorithmTag tag = _pgpKeys.SecretKey.PublicKey.Algorithm;
-			var pgpSignatureGenerator = new PgpSignatureGenerator(tag, HashAlgorithmTag.Sha1);
-			pgpSignatureGenerator.InitSign(PgpSignature.BinaryDocument, _pgpKeys.PrivateKey);
-
-			// Get the user identity from our secret key and use it for signing
-			var userIds = _pgpKeys.SecretKey.PublicKey.GetUserIds().OfType<string>();
-			string userId = userIds.FirstOrDefault();
-			if (userId != null)
-			{
-				var subPacketGenerator = new PgpSignatureSubpacketGenerator();
-				subPacketGenerator.SetSignerUserId(isCritical: false, userId: userId);
-				pgpSignatureGenerator.SetHashedSubpackets(subPacketGenerator.Generate());
-			}
-
-			// Kick things off and send the generator back to caller for the rest
-			pgpSignatureGenerator.GenerateOnePassVersion(isNested: false).Encode(streamToSign);
-			return pgpSignatureGenerator;
-		}
 	}
 }
